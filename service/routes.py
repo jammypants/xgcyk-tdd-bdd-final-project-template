1fb0c8e2cddc4999f3a6b2256cf9b21aded163f5
Avi <100024709+jammypants@users.noreply.github.com>
2024-11-07 18:03:51 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -1,128 +1,61 @@
-######################################################################
-# Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-######################################################################
+from flask import Flask, jsonify, request
 
-# spell: ignore Rofrano jsonify restx dbname
-"""
-Product Store Service with UI
-"""
-from flask import jsonify, request, abort
-from flask import url_for  # noqa: F401 pylint: disable=unused-import
-from service.models import Product
-from service.common import status  # HTTP Status Codes
-from . import app
+app = Flask(__name__)
 
+# Sample in-memory data store
+items = [
+    {"id": 1, "name": "Item1", "category": "CategoryA", "available": True},
+    {"id": 2, "name": "Item2", "category": "CategoryB", "available": False},
+    # Add more items as needed
+]
 
-######################################################################
-# H E A L T H   C H E C K
-######################################################################
-@app.route("/health")
-def healthcheck():
-    """Let them know our heart is still beating"""
-    return jsonify(status=200, message="OK"), status.HTTP_200_OK
+# 1. List All Items
+@app.route('/items', methods=['GET'])
+def list_all_items():
+    return jsonify(items), 200
 
+# 2. List Items by Name
+@app.route('/items/name/<string:name>', methods=['GET'])
+def list_items_by_name(name):
+    filtered_items = [item for item in items if item['name'] == name]
+    return jsonify(filtered_items), 200 if filtered_items else 404
 
-######################################################################
-# H O M E   P A G E
-######################################################################
-@app.route("/")
-def index():
-    """Base URL for our service"""
-    return app.send_static_file("index.html")
+# 3. List Items by Category
+@app.route('/items/category/<string:category>', methods=['GET'])
+def list_items_by_category(category):
+    filtered_items = [item for item in items if item['category'] == category]
+    return jsonify(filtered_items), 200 if filtered_items else 404
 
+# 4. List Items by Availability
+@app.route('/items/availability/<string:availability>', methods=['GET'])
+def list_items_by_availability(availability):
+    is_available = availability.lower() == 'true'
+    filtered_items = [item for item in items if item['available'] == is_available]
+    return jsonify(filtered_items), 200 if filtered_items else 404
 
-######################################################################
-#  U T I L I T Y   F U N C T I O N S
-######################################################################
-def check_content_type(content_type):
-    """Checks that the media type is correct"""
-    if "Content-Type" not in request.headers:
-        app.logger.error("No Content-Type specified.")
-        abort(
-            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            f"Content-Type must be {content_type}",
-        )
+# 5. Read Item by ID
+@app.route('/items/<int:item_id>', methods=['GET'])
+def read_item(item_id):
+    item = next((item for item in items if item['id'] == item_id), None)
+    return jsonify(item), 200 if item else 404
 
-    if request.headers["Content-Type"] == content_type:
-        return
+# 6. Update Item by ID
+@app.route('/items/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    data = request.json
+    item = next((item for item in items if item['id'] == item_id), None)
+    if not item:
+        return jsonify({"message": "Item not found"}), 404
 
-    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
-    abort(
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-        f"Content-Type must be {content_type}",
-    )
+    item.update(data)
+    return jsonify(item), 200
 
+# 7. Delete Item by ID
+@app.route('/items/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    global items
+    items = [item for item in items if item['id'] != item_id]
+    return jsonify({"message": "Item deleted"}), 200
 
-######################################################################
-# C R E A T E   A   N E W   P R O D U C T
-######################################################################
-@app.route("/products", methods=["POST"])
-def create_products():
-    """
-    Creates a Product
-    This endpoint will create a Product based the data in the body that is posted
-    """
-    app.logger.info("Request to Create a Product...")
-    check_content_type("application/json")
-
-    data = request.get_json()
-    app.logger.info("Processing: %s", data)
-    product = Product()
-    product.deserialize(data)
-    product.create()
-    app.logger.info("Product with new id [%s] saved!", product.id)
-
-    message = product.serialize()
-
-    #
-    # Uncomment this line of code once you implement READ A PRODUCT
-    #
-    # location_url = url_for("get_products", product_id=product.id, _external=True)
-    location_url = "/"  # delete once READ is implemented
-    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
-
-
-######################################################################
-# L I S T   A L L   P R O D U C T S
-######################################################################
-
-#
-# PLACE YOUR CODE TO LIST ALL PRODUCTS HERE
-#
-
-######################################################################
-# R E A D   A   P R O D U C T
-######################################################################
-
-#
-# PLACE YOUR CODE HERE TO READ A PRODUCT
-#
-
-######################################################################
-# U P D A T E   A   P R O D U C T
-######################################################################
-
-#
-# PLACE YOUR CODE TO UPDATE A PRODUCT HERE
-#
-
-######################################################################
-# D E L E T E   A   P R O D U C T
-######################################################################
-
-
-#
-# PLACE YOUR CODE TO DELETE A PRODUCT HERE
-#
+if __name__ == '__main__':
+    app.run(debug=True)
